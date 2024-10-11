94e0a2a4e453b682576aab3a92bcaeac78cdc8b4
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-10-10 21:07:41 -0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Jason Cox on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
