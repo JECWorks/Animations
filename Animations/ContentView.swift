b032bc3594ea3ff6769f28f065f8961a3a933b94
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-10-16 22:03:27 -0400

    Building custom transitions using ViewModifier

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -7,6 +7,25 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
     @State private var isShowingRed = false
     let letters = Array("Hello SwiftUI")
@@ -15,6 +34,23 @@ struct ContentView: View {
     @State private var dragAmount = CGSize.zero
     
     var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation{
+                isShowingRed.toggle()
+            }
+        }
 //        HStack(spacing: 0) {
 //            ForEach(0..<letters.count, id: \.self) { num in
 //                Text(String(letters[num]))
@@ -33,21 +69,23 @@ struct ContentView: View {
 //                    enabled.toggle()
 //                }
 //        )
-        VStack {
-            Button("Tap me") {
-                withAnimation {
-                isShowingRed.toggle()
-                }
-            }
-            
-            if isShowingRed {
-                Rectangle()
-                    .fill(.red)
-                    .frame(width: 200, height: 200)
-//                    .transition(.scale)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
-            }
-        }
+// -----------------------------
+//        VStack {
+//            Button("Tap me") {
+//                withAnimation {
+//                isShowingRed.toggle()
+//                }
+//            }
+//            
+//            if isShowingRed {
+//                Rectangle()
+//                    .fill(.red)
+//                    .frame(width: 200, height: 200)
+////                    .transition(.scale)
+//                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+//            }
+//        }
+        
     }
 }
 
